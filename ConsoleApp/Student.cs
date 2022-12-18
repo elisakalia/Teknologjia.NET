2ce735ea927d61681cc774ba4926c08bf45a171e
Elisa Kalia <kaliaelisa0@gmail.com>
2022-12-18 22:54:01 +0100

    initial commit

---
+++ ConsoleApp/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(120)]
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int GetAge(DateTime birthDate)
+        {
+            //Calculate Age
+            return 10;
+        }
+
+        //Comment added on 05/12/2022
+
+    }
+}
